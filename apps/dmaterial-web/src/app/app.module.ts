87f16942d03c21e5e30b0504cb54e7ef4acc5c39
Diego Vieira <diegoavieira@gmail.com>
2019-11-13 14:28:34 -0300

    created page dashboard

--- apps/dmaterial-web/src/app/app.module.ts
+++ apps/dmaterial-web/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('@dmaterial/pages').then(module => module.DmDashboardModule)
+  }
+];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' })
   ],
   providers: [],
   bootstrap: [AppComponent]
